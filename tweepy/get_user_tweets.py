d7e62eda5b767aa662d57daf0cf65132f5e44941
Cameron Raymond <15cjkr@queensu.ca>
2019-09-11 18:35:27 -0400

    allow longer tweets

--- tweepy/get_user_tweets.py
+++ tweepy/get_user_tweets.py
@@ -15,23 +15,20 @@ CONSUMER_SECRET = login['CONSUMER_SECRET']
 ACCESS_KEY      = login['ACCESS_KEY']
 ACCESS_SECRET   = login['ACCESS_SECRET']
 
-def get_all_tweets(screen_name):
+def get_all_tweets(api,screen_name):
 	#Twitter only allows access to a users most recent 3240 tweets with this method
 	#authorize twitter, initialize tweepy
-	print("authorize twitter, initialize tweepy")
-	auth 		= tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-	api 		= tweepy.API(auth)
 	num_tweets	= 4000
-	tweets 		= tweepy.Cursor(api.user_timeline,screen_name=screen_name,count=num_tweets,include_rts=False)
+	print("--- Return Tweets for {} ---".format(screen_name))
+	tweets 		= tweepy.Cursor(api.user_timeline,screen_name=screen_name,count=num_tweets,include_rts=False,tweet_mode='extended')
 	timeline_df = pd.DataFrame(columns=COLS)
+	print("--- Clean Data ---")
 	for tweet in tweets.items():
 		if tweet.lang == 'en':
 			tweet_df = clean_tweet(tweet)
 			timeline_df = timeline_df.append(tweet_df, ignore_index=True)
 
 	return timeline_df
-		
 		
 def clean_tweet(tweet_obj):
 	cleaned_tweet 	= []
@@ -47,26 +44,24 @@ def clean_tweet(tweet_obj):
 
 def write_to_file(file_path,new_data):
 	data_frame = new_data
-	# print(data_frame.to_string())
-	# if os.path.exists(file_path):
-	# 	data_frame = pd.read_csv(file_path,header=0)
-	# 	data_frame.append(new_data)
-	
 	csvFile = open(file_path, 'a' ,encoding='utf-8')
 	data_frame.to_csv(csvFile, mode='a', columns=COLS, index=False, encoding="utf-8")
 
 if __name__ == '__main__':
-	lib_tweets = "../data/liberal_party_data.csv"
-	trudeau_tweets = "../data/trudeau_data.csv"
+	lib_tweets		= "../data/liberal_party_data.csv"
+	trudeau_tweets 	= "../data/trudeau_data.csv"
 	try:
 		os.remove(lib_tweets)
 		os.remove(trudeau_tweets)
 	except:
 		pass
-
+	print("--- Authorize Twitter; Initialize Tweepy ---")
+	auth 		= tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+	api 		= tweepy.API(auth)
 	#pass in the username of the account you want to download
-	lib_df = get_all_tweets("liberal_party")
-	trudeau_df = get_all_tweets("JustinTrudeau")
+	lib_df = get_all_tweets(api,"liberal_party")
+	trudeau_df = get_all_tweets(api,"JustinTrudeau")
 	write_to_file(lib_tweets,lib_df)
 	write_to_file(trudeau_tweets,trudeau_df)
 
