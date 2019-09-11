60c2bc981db3c5b8dd16a50a1d1b71a4a5e65a95
Cameron Raymond <15cjkr@queensu.ca>
2019-09-11 18:19:44 -0400

    working on data cleaning

--- tweepy/get_user_tweets.py
+++ tweepy/get_user_tweets.py
@@ -1,6 +1,7 @@
 import os
 import tweepy
-import cred
+import emoji
+from cred import login
 from tweet_config import COLS
 import pandas as pd
 import preprocessor as p
@@ -8,23 +9,24 @@ import re #regular expression
 import json
 import csv
 
-CONSUMER_KEY    = cred.login['CONSUMER_KEY']
-CONSUMER_SECRET = cred.login['CONSUMER_SECRET']
-ACCESS_KEY      = cred.login['ACCESS_KEY']
-ACCESS_SECRET   = cred.login['ACCESS_SECRET']
+p.set_options(p.OPT.URL,p.OPT.SMILEY)
+CONSUMER_KEY    = login['CONSUMER_KEY']
+CONSUMER_SECRET = login['CONSUMER_SECRET']
+ACCESS_KEY      = login['ACCESS_KEY']
+ACCESS_SECRET   = login['ACCESS_SECRET']
 
 def get_all_tweets(screen_name):
 	#Twitter only allows access to a users most recent 3240 tweets with this method
-	
 	#authorize twitter, initialize tweepy
+	print("authorize twitter, initialize tweepy")
 	auth 		= tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 	auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 	api 		= tweepy.API(auth)
-	num_tweets	= 200
-	tweets 		= api.user_timeline(screen_name=screen_name,count=3000)
+	num_tweets	= 4000
+	tweets 		= tweepy.Cursor(api.user_timeline,screen_name=screen_name,count=num_tweets,include_rts=False)
 	timeline_df = pd.DataFrame(columns=COLS)
-	for tweet in tweets:
-		if (not tweet.retweeted) and ('RT @' not in tweet.text):
+	for tweet in tweets.items():
+		if tweet.lang == 'en':
 			tweet_df = clean_tweet(tweet)
 			timeline_df = timeline_df.append(tweet_df, ignore_index=True)
 
@@ -34,7 +36,8 @@ def get_all_tweets(screen_name):
 def clean_tweet(tweet_obj):
 	cleaned_tweet 	= []
 	tweet			= tweet_obj._json
-	cleaned_text 	= p.clean(tweet['text'])
+	raw_text		= emoji.demojize(tweet['text'])
+	cleaned_text 	= p.clean(raw_text)
 	cleaned_tweet 	+= [tweet['id'], tweet['created_at'],tweet['source'], tweet['text'],cleaned_text, tweet['lang'],tweet['favorite_count'], tweet['retweet_count']]
 	hashtags = ", ".join([hashtag_item['text'] for hashtag_item in tweet['entities']['hashtags']])
 	cleaned_tweet.append(hashtags) #append the hashtags
@@ -55,6 +58,12 @@ def write_to_file(file_path,new_data):
 if __name__ == '__main__':
 	lib_tweets = "../data/liberal_party_data.csv"
 	trudeau_tweets = "../data/trudeau_data.csv"
+	try:
+		os.remove(lib_tweets)
+		os.remove(trudeau_tweets)
+	except:
+		pass
+
 	#pass in the username of the account you want to download
 	lib_df = get_all_tweets("liberal_party")
 	trudeau_df = get_all_tweets("JustinTrudeau")
--- tweepy/tweet_config.py
+++ tweepy/tweet_config.py
@@ -1,3 +1,7 @@
+import preprocessor as p
+
 COLS = ['id', 'created_at', 'source', 'original_text','clean_text', 'lang',
 'favorite_count', 'retweet_count', 
 'hashtags', 'original_author',]
+
+preprocessor = p.set_options(p.OPT.NUMBER)
